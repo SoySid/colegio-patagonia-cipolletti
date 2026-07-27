3be5ff2b1ccef7c4d79df5827f7a937cc22a0048
Alfredo Lopez <soysidarg@gmail.com>
2026-07-26 22:01:00 -0300

    feat(escuela): implementar herencia de templates y diseño css moderno

--- escuela/views.py
+++ escuela/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Página Institucional de la Escuela</h1><p>Bienvenidos al sitio web oficial.</p>")
+    return render(request, 'escuela/home.html')
 
 def contacto(request):
-    return HttpResponse("<h1>Contacto</h1><p>Consultas: contacto@escuela.edu.ar</p>")
+    return render(request, 'escuela/contacto.html')
 
 def portal_login(request):
-    return HttpResponse("<h1>Portal Alumnos</h1><p>Ingreso con usuario y contraseña.</p>")
+    return render(request, 'escuela/portal.html')
