e8c25346d9b1c61c9089eaca4fdbfb7ee7e56607
Alfredo Lopez <soysidarg@gmail.com>
2026-07-27 00:03:57 -0300

    feat(requisitos): agregar opcion de ambos niveles para requisitos compartidos

--- escuela/models.py
+++ escuela/models.py
@@ -2,10 +2,23 @@ from django.db import models
 
 class Noticia(models.Model):
     titulo = models.CharField(max_length=200)
-    categoria = models.CharField(max_length=50)  # Ej: Admisiones, Tecnología, Comunidad
+    categoria = models.CharField(max_length=50)
     descripcion = models.TextField()
     imagen_url = models.URLField(max_length=500, blank=True, null=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.titulo
+
+class Requisito(models.Model):
+    NIVELES = [
+        ('PRIMARIO', 'Solo Nivel Primario'),
+        ('SECUNDARIO', 'Solo Nivel Secundario'),
+        ('AMBOS', 'Ambos Niveles (General)'),
+    ]
+
+    nivel = models.CharField(max_length=20, choices=NIVELES, default='AMBOS')
+    descripcion = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.get_nivel_display()} - {self.descripcion}"
--- escuela/views.py
+++ escuela/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Noticia
+from django.db.models import Q
+from .models import Noticia, Requisito
 
 def home(request):
     noticias = Noticia.objects.all().order_by('-fecha_creacion')
@@ -12,4 +13,13 @@ def portal_login(request):
     return render(request, 'escuela/portal.html')
 
 def requisitos(request):
-    return render(request, 'escuela/requisitos.html')
+    # Trae los específicos de Primario MÁS los que son para Ambos
+    req_primario = Requisito.objects.filter(Q(nivel='PRIMARIO') | Q(nivel='AMBOS'))
+    
+    # Trae los específicos de Secundario MÁS los que son para Ambos
+    req_secundario = Requisito.objects.filter(Q(nivel='SECUNDARIO') | Q(nivel='AMBOS'))
+
+    return render(request, 'escuela/requisitos.html', {
+        'req_primario': req_primario,
+        'req_secundario': req_secundario
+    })
