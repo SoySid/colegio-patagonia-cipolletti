d60438acd9a57d130ea87d7c055619ff5d5f1e31
Alfredo Lopez <soysidarg@gmail.com>
2026-07-26 21:52:24 -0300

    feat(escuela): agregar vistas y rutas iniciales para home, contacto y portal

--- escuela/views.py
+++ escuela/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("<h1>Página Institucional de la Escuela</h1><p>Bienvenidos al sitio web oficial.</p>")
+
+def contacto(request):
+    return HttpResponse("<h1>Contacto</h1><p>Consultas: contacto@escuela.edu.ar</p>")
+
+def portal_login(request):
+    return HttpResponse("<h1>Portal Alumnos</h1><p>Ingreso con usuario y contraseña.</p>")
