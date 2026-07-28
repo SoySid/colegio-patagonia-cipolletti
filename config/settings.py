2fc7ddd5ef6e1128bd4b033f85cbf8d85d0f6e3e
Alfredo Lopez <soysidarg@gmail.com>
2026-07-28 18:56:31 -0300

    feat: integracion con cloudinary para imagenes

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'escuela',
 ]
 
@@ -107,3 +109,11 @@ JAZZMIN_SETTINGS = {
     "welcome_sign": "Bienvenido al Sistema del Colegio Patagonia Cipolletti",
     "copyright": "Colegio Patagonia Cipolletti",
 }
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
